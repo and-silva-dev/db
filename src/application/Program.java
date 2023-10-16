91190ddaf0bf249f397831f2240ea31b429bdc4a
Anderson Silva <andersonsoufeliz27@gmail.com>
2023-10-16 18:16:35 -0300

    Atualizar os dados

--- src/application/Program.java
+++ src/application/Program.java
@@ -13,35 +13,17 @@ public class Program {
 
     public static void main(String[] args) {
         // Variáveis
-        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
         Connection conn = null;
         PreparedStatement st = null;
-
         try {
             conn = DB.getConnection();
+            st = conn.prepareStatement("UPDATE seller SET BaseSalary = BaseSalary + ? WHERE (DepartmentId = ?)");
 
-            st = conn.prepareStatement("INSERT INTO seller (Name , Email, BirthDate , BaseSalary , DepartmentId) VALUES(?, ?, ?, ?, ? ) ",
-                    Statement.RETURN_GENERATED_KEYS);
-
-            st.setString(1, "Carl Purple");
-            st.setString(2, "carl@gmail.com");
-            st.setString(3, "carl@gmail.com");
-            st.setDate(3, new java.sql.Date(sdf.parse("24/11/2023").getTime()));
-            st.setDouble(4, 3000.0);
-            st.setDouble(5, 4);
-            int rowsAffected = st.executeUpdate(); // Comando usado para fazer a inserção de dados .
-            if (rowsAffected > 0) {
-                ResultSet rs = st.getGeneratedKeys();
-                while (rs.next()){
-                int id = rs.getInt(1);
-                    System.out.println("Done! Id = " + id);
-            }
-                System.out.println("No rown affected!");
-            }
-
+            st.setDouble(1, 200.0);
+            st.setInt(2, 2);
+            int rowsAffected = st.executeUpdate();
+            System.out.println("Done! Rows affected: " + rowsAffected);
         } catch (SQLException e) {
-            e.printStackTrace();
-        } catch (ParseException e) {
             e.printStackTrace();
         } finally {
             DB.closeStatement(st);
